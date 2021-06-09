e936976746ae2cd411fcc88133f3541d8ce57395
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-06-09 14:41:47 -0400

    Added bootstrap5

--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,7 @@ const Layout = ({ children, title }) => {
       <Head>
         <title>{title}</title>
         <link rel='icon' href='/bootstrap5.png' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
 
       <div>{children}</div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Layout from '../components/Layout';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 
 const Home = () => {
   return <Layout title='Bootstrap 5 Course'>Bootstrap 5</Layout>;
