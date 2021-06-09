c5808abb83ce54735ed81858d3b45ac16426abb8
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-06-09 14:46:39 -0400

    Added bootstrap script file

--- components/Layout.js
+++ components/Layout.js
@@ -5,8 +5,9 @@ const Layout = ({ children, title }) => {
     <div>
       <Head>
         <title>{title}</title>
-        <link rel='icon' href='/bootstrap5.png' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/bootstrap5.png' />
+        <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js'></script>
       </Head>
 
       <div>{children}</div>
