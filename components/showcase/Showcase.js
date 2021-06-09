97e02b2fc262a7bfae4321a0a55e443241cf2053
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-06-09 15:24:43 -0400

    Completed Showcase component

---
+++ components/showcase/Showcase.js
@@ -0,0 +1,28 @@
+const Showcase = () => {
+  return (
+    <section className='bg-dark text-light p-5 text-center'>
+      <div className='container'>
+        <div className='d-sm-flex align-items-sm-center text-sm-start'>
+          <div>
+            <h1>
+              Become A <span className='text-warning'>Web Developer</span>
+            </h1>
+            <p className='lead my-4'>
+              We focus on teaching our students the fundamentals of the latest
+              and greatest technologies to prepare them for their first dev
+              role.
+            </p>
+            <button className='btn btn-primary btn-lg'>Start Enrollment</button>
+          </div>
+          <img
+            className='d-none d-sm-block img-fluid w-50'
+            src='/images/showcase.svg'
+            alt='Showcase'
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Showcase;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Layout from '../components/Layout';
+import Showcase from '../components/showcase/Showcase';
 import styles from '../styles/Home.module.scss';
 
 const Home = () => {
-  return <Layout title='Bootstrap 5 Course'>Bootstrap 5</Layout>;
+  return (
+    <Layout title='Bootstrap 5 Course'>
+      <Showcase />
+    </Layout>
+  );
 };
 
 export default Home;
