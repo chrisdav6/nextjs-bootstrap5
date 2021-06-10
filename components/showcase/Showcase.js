fde927e0b49d9bb526cff1026b54432c8a105a27
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-06-10 13:11:13 -0400

    Completed modal

--- components/showcase/Showcase.js
+++ components/showcase/Showcase.js
@@ -1,27 +1,103 @@
 const Showcase = () => {
   return (
-    <section className='bg-dark text-light p-5 pb-lg-0 text-center'>
-      <div className='container'>
-        <div className='d-sm-flex align-items-sm-center text-sm-start'>
-          <div>
-            <h1>
-              Become A <span className='text-warning'>Web Developer</span>
-            </h1>
-            <p className='lead my-4'>
-              We focus on teaching our students the fundamentals of the latest
-              and greatest technologies to prepare them for their first dev
-              role.
-            </p>
-            <button className='btn btn-primary btn-lg'>Start Enrollment</button>
+    <>
+      <section className='bg-dark text-light p-5 pb-lg-0 text-center'>
+        <div className='container'>
+          <div className='d-sm-flex align-items-sm-center text-sm-start'>
+            <div>
+              <h1>
+                Become A <span className='text-warning'>Web Developer</span>
+              </h1>
+              <p className='lead my-4'>
+                We focus on teaching our students the fundamentals of the latest
+                and greatest technologies to prepare them for their first dev
+                role.
+              </p>
+              <button
+                className='btn btn-primary btn-lg'
+                data-bs-toggle='modal'
+                data-bs-target='#enroll'
+              >
+                Start Enrollment
+              </button>
+            </div>
+            <img
+              className='d-none d-sm-block img-fluid w-50'
+              src='/images/showcase.svg'
+              alt='Showcase'
+            />
           </div>
-          <img
-            className='d-none d-sm-block img-fluid w-50'
-            src='/images/showcase.svg'
-            alt='Showcase'
-          />
+        </div>
+      </section>
+
+      {/*Modal*/}
+      <div
+        class='modal fade'
+        id='enroll'
+        tabindex='-1'
+        aria-labelledby='exampleModalLabel'
+        aria-hidden='true'
+      >
+        <div class='modal-dialog modal-dialog-centered'>
+          <div class='modal-content'>
+            <div class='modal-header'>
+              <h5 class='modal-title' id='exampleModalLabel'>
+                Enroll Today!
+              </h5>
+              <button
+                type='button'
+                class='btn-close'
+                data-bs-dismiss='modal'
+                aria-label='Close'
+              ></button>
+            </div>
+            <div class='modal-body'>
+              <p className='lead'>
+                Complete the form and we will get back to you.
+              </p>
+              <form>
+                <div className='mb-2'>
+                  <label htmlFor='first-name' className='col-form-label'>
+                    First Name
+                  </label>
+                  <input type='text' className='form-control' id='first-name' />
+                </div>
+                <div className='mb-2'>
+                  <label htmlFor='last-name' className='col-form-label'>
+                    Last Name
+                  </label>
+                  <input type='text' className='form-control' id='last-name' />
+                </div>
+                <div className='mb-2'>
+                  <label htmlFor='email' className='col-form-label'>
+                    Email
+                  </label>
+                  <input type='email' className='form-control' id='email' />
+                </div>
+                <div className='mb-3'>
+                  <label htmlFor='phone' className='col-form-label'>
+                    Phone Number
+                  </label>
+                  <input type='tel' className='form-control' id='phone' />
+                </div>
+              </form>
+            </div>
+            <div class='modal-footer'>
+              <button
+                type='button'
+                class='btn btn-secondary'
+                data-bs-dismiss='modal'
+              >
+                Close
+              </button>
+              <button type='button' class='btn btn-primary'>
+                Submit
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </section>
+    </>
   );
 };
 
